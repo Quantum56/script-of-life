da72d4540fe3a17adc167b3a20a19b5f21eeef49
Zack Bartley <Quantum56@users.noreply.github.com>
2019-12-28 17:59:51 -0500

    the script of life

---
+++ life.c
@@ -0,0 +1,9 @@
+void main() {
+	for(i = 0 knowledge; i < infinite knowledge; i++) 
+		while(you don't know how something works) { 
+			Read(Your Brain, i); 
+			Experiment(Your Brain, i); 
+			Learn(Your Brain, i); 
+		}
+	}
+} // the program of life
